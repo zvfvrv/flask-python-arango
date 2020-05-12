da20696e1dd9efb2b8415252698dca9ee32ceb28
Francesco Lombardo <zvfvrv@users.noreply.github.com>
2020-05-12 11:15:37 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="Flask-Python-Arango",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/zvfvrv/flask-python-arango",
     download_url="https://github.com/zvfvrv/flask-python-arango/tags",
     license="Apache-2.0",
