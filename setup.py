473db843dcb7b46652664b43a00feadc44bca229
Francesco Lombardo <zvfvrv@users.noreply.github.com>
2020-05-12 11:19:58 +0200

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Contributions are welcome!
 
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
